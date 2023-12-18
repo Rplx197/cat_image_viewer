b7fe0f0caa5ac0100260c63d9bef6904d05ec3cc
Rplx197 <brafli99197@gmail.com>
2023-12-18 15:49:48 +0700

    coba lagi

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cat Image Viewer',
+      title: 'Random Cat',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
